0e3bbfaa29cfc5736b8247c3a443cbe56223b639
Adam Wilder <awilder@arrivelogistics.com>
2021-03-10 17:46:28 -0600

    try naming repository with datasource name

--- src/main/kotlin/com/example/TestEntityRepository.kt
+++ src/main/kotlin/com/example/TestEntityRepository.kt
@@ -3,5 +3,5 @@ package com.example
 import io.micronaut.data.annotation.Repository
 import io.micronaut.data.repository.reactive.RxJavaCrudRepository
 
-@Repository
+@Repository("testdatasource")
 interface TestEntityRepository : RxJavaCrudRepository<TestEntity, Int>
