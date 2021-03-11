1180e851ddfeca7193d82d8dc971f814b0062a71
Adam Wilder <awilder@arrivelogistics.com>
2021-03-11 11:05:13 -0600

    2.3.0 version

--- src/main/kotlin/com/example/TestEntityRepository.kt
+++ src/main/kotlin/com/example/TestEntityRepository.kt
@@ -3,5 +3,5 @@ package com.example
 import io.micronaut.data.annotation.Repository
 import io.micronaut.data.repository.reactive.RxJavaCrudRepository
 
-@Repository("testdatasource")
+@Repository(value = "testdatasource")
 interface TestEntityRepository : RxJavaCrudRepository<TestEntity, Int>
